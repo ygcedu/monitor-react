1a7244341038359eb261905a33ea28ec05ff8cf9
ygcedu <937601471@qq.com>
2021-11-04 00:08:55 +0800

    重构提取出 create-echarts-options.ts

--- src/components/chart-1.tsx
+++ src/components/chart-1.tsx
@@ -1,19 +1,12 @@
 import React, {useEffect, useRef} from 'react';
 import * as echarts from 'echarts';
-
-const px = (n) => n / 2420 * (window as any).pageWidth;
+import {createEchartsOptions} from '../shared/create-echarts-options';
 
 export const Chart1 = () => {
   const divRef = useRef(null);
   useEffect(() => {
     var myChart = echarts.init(divRef.current);
-    myChart.setOption({
-      textStyle: {
-        fontSize: px(12),
-        color: '#79839E'
-      },
-      title: {show: false},
-      legend: {show: false},
+    myChart.setOption(createEchartsOptions({
       xAxis: {
         data: ['兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区'],
         axisTick: {show: false},
@@ -21,8 +14,6 @@ export const Chart1 = () => {
           lineStyle: {color: '#083B70'}
         },
         axisLabel: {
-          fontSize: px(12),
-          // label 换行
           formatter(val) {
             if (val.length > 2) {
               const array = val.split('');
@@ -34,33 +25,27 @@ export const Chart1 = () => {
           }
         },
       },
-      grid: {
-        x: px(40),
-        y: px(40),
-        x2: px(40),
-        y2: px(40),
-      },
+
       yAxis: {
         splitLine: {show: false},
         axisLine: {
           show: true,
           lineStyle: {color: '#083B70'}
-        },
-        axisLabel: {
-          fontSize: px(12)
         }
       },
       series: [{
         type: 'bar',
         data: [10, 20, 36, 41, 15, 26, 37, 18, 29]
       }]
-    });
+    }));
   }, []);
 
   return (
     <div className="bordered 管辖统计">
       <h2>案发派出所管辖统计</h2>
-      <div ref={divRef} className="chart"></div>
+      <div ref={divRef} className="chart">
+
+      </div>
     </div>
   );
 };
