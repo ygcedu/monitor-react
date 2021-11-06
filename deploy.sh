dda984a09c6ebd76822b3a35d450fdca918f201a
ygcedu <937601471@qq.com>
2021-11-06 10:42:31 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
 yarn build --base ./
-#git add dist -f && git commit -m "deploy"
+git add dist -f && git commit -m "deploy"
 git subtree push --prefix dist origin gh-pages
 echo https://ygcedu.github.io/monitor-react/index.html
